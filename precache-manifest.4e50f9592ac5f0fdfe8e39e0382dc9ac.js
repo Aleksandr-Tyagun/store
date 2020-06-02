62ad7033856509e41ea36e51c8fe69e85fcf4743
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-06-02 21:35:47 +0300

    Updates

---
+++ precache-manifest.4e50f9592ac5f0fdfe8e39e0382dc9ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4036697e494f229c7c9c4452c7b08dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae88653f5d39a056d5ea",
+    "url": "./static/css/main.2c847ed8.chunk.css"
+  },
+  {
+    "revision": "f07c6ec341ae12b421db",
+    "url": "./static/js/2.dee98294.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dee98294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae88653f5d39a056d5ea",
+    "url": "./static/js/main.4f499e62.chunk.js"
+  },
+  {
+    "revision": "cd7566fc37b0e6b39b5e",
+    "url": "./static/js/runtime-main.24dcbce3.js"
+  }
+]);
