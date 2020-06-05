dbe19abe00d9b704daa46cc3ae1b8e688a99ba3c
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-06-05 12:38:02 +0300

    replaced filtering to the store

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,46 +1,12 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 
 import './ProductsList.scss';
 import * as store from '../store';
 import ProductCard from './ProductCard';
-import { SORT_BY, CATEGORY } from '../constants';
 
 const ProductsList = () => {
-  const products: Product[] = useSelector(store.getProducts);
-  const sortBy = useSelector(store.getSortBy);
-  const filterBy = useSelector(store.getFilterBy);
-
-  const visibleProducts = useMemo(() => {
-    const result = products.filter(item => {
-      if(filterBy !== '' && filterBy !== CATEGORY.all) {
-        return item.category === filterBy
-      }
-
-      return item
-    });
-
-    switch (sortBy) {
-      case SORT_BY.priceAsc:
-        return result
-          .sort((a, b) => a.price - b.price);
-
-      case SORT_BY.priceDesc:
-        return result
-          .sort((a, b) => b.price - a.price);
-
-      case SORT_BY.popular:
-        return result
-          .sort((a, b) => b.reviews - a.reviews);
-
-      case SORT_BY.new:
-        return result
-          .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
-
-      default:
-        return result;
-    }
-  }, [filterBy, sortBy, products]);
+  const visibleProducts = useSelector(store.getVisibleProducts);
 
   return (
     <>
--- src/store/index.ts
+++ src/store/index.ts
@@ -11,6 +11,7 @@ import errorReducer from './error';
 import cartReducer from './cart';
 import sidebarReducer from './sidebar';
 import filterReducer from './filter';
+import { CATEGORY, SORT_BY } from '../constants';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -42,6 +43,37 @@ export const loadProducts = () => {
     }
   };
 };
+
+export const getVisibleProducts = (state: RootState) => {
+  const visibleProducts: Product[] = state.products.filter((item: Product) => {
+    if(state.filterBy !== '' && state.filterBy !== CATEGORY.all) {
+      return item.category === state.filterBy
+    }
+
+    return item
+  });
+
+  switch (state.sortBy) {
+    case SORT_BY.priceAsc:
+      return visibleProducts
+        .sort((a, b) => a.price - b.price);
+
+    case SORT_BY.priceDesc:
+      return visibleProducts
+        .sort((a, b) => b.price - a.price);
+
+    case SORT_BY.popular:
+      return visibleProducts
+        .sort((a, b) => b.reviews - a.reviews);
+
+    case SORT_BY.new:
+      return visibleProducts
+        .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+
+    default:
+      return visibleProducts;
+  }
+}
 
 const store = createStore(
   rootReducer,
